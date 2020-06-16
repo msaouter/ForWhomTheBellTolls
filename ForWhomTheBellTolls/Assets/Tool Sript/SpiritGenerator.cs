0cf394355dc3f814c66c887171bc28d39fc4a96f
Laurianne <Laurianne.espinadel@orange.fr>
2020-06-16 19:32:04 +0200

    OskurGenHéRal

--- ForWhomTheBellTolls/Assets/Tool Sript/SpiritGenerator.cs
+++ ForWhomTheBellTolls/Assets/Tool Sript/SpiritGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -178,3 +179,4 @@ public class SpiritGenerator : ScriptableObject
         }
     }
 }
+#endif
--- ForWhomTheBellTolls/Assets/Tool Sript/SpiritLogger.cs
+++ ForWhomTheBellTolls/Assets/Tool Sript/SpiritLogger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -30,6 +31,5 @@ public class SpiritLogger : EditorWindow
             generator.Generate(csvFileName, addInScene, defaltTime);
         }
     }
-
-    
 }
+#endif
